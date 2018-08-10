9070c0198d239f5fd82a89de61b67a4b5b425cda
ealunos <40867318+ealunos@users.noreply.github.com>
2018-08-10 20:56:08 -0300

    Atualização dia 10 - 08 2018

---
+++ Aula02/js/variaveis.js
@@ -0,0 +1,17 @@
+var nome='Curso';
+var NOME='Turma C';
+
+nome='pronatec';
+
+
+//algoritmo 01
+let nota_Aval_01=5,nota_Aval_02=8,notaTrabalho=3,media=0;
+
+/*
+var nota_Aval_01=8
+var nota_Aval_02=10;
+var notaTrabalho=10;
+var media=0;*/
+
+
+media=(nota_Aval_01+nota_Aval_02+notaTrabalho)/3;
---
+++ js/exemplo01aula01.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+/*Code para mostrar uma janela com mensagem
+
+        alert('Oi.. eu sou JavaScript. ');
+
+Code escreve um frase no documento html*/
+
+function elton(){
+document.write( "sou um texto em HTML");
+}
+
+var x=10;
+var y=50;
+function chamavar(){
+  document.write("valor de X: "+x+" valor de y;= "+y); 
+   var z=x+y;
+   document.write("O resultado da soma é:= "+z); 
+}
+
+var nome='Aparecido';
+var sobrenome="Alves";
+function escrevenome(){
+  document.write("Seu nome é "+nome+" e seu sobronome é "+sobrenome); 
+   
+}
